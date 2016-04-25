798c08f0cbe0f5465edf25c960484da70b44596a
Eric Dobbs <eric@dobbse.net>
2016-04-25 08:55:41 -0600

    vault needs an init process so adding tini (statically compiled)
    We're using golang-builder to have only vault in the container.
    Need an init process to handle PID1 obligations.  Tini offers
    that.  And we need the statically compiled version of it 'cos
    we've left everything else out of this container.

--- vault/build_environment.sh
+++ vault/build_environment.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+TINI_VERSION=v0.9.0
+curl -Lo /src/tini https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini-static\
+ && curl -Lo /src/tini.asc https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini.asc-static\
+ && gpg --keyserver ha.pool.sks-keyservers.net --recv-keys 0527A9B7 \
+ && gpg --verify /src/tini.asc \
+ && chmod +x /src/tini
+
 curl -L https://github.com/hashicorp/vault/archive/v0.5.2.tar.gz \
     | tar zxf - -C /src --strip-components=1
 
